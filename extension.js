004b2c4d681fda7f880154255cd83b2bfdb1c64e
arelpha24 <64937615+arelpha24@users.noreply.github.com>
2023-05-24 21:15:46 -0400

    Autocompletion for patterns

--- extension.js
+++ extension.js
@@ -6,13 +6,15 @@ const path = require('path');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-    // Read the JSON file
+    // Read the JSON files
     const functionsWithParameters = JSON.parse(fs.readFileSync(path.join(__dirname, 'karelFunctions.json'), 'utf-8'));
+    const patterns = JSON.parse(fs.readFileSync(path.join(__dirname, 'karelPatterns.json'), 'utf-8'));
 
     context.subscriptions.push(
         vscode.languages.registerCompletionItemProvider('karel', {
             provideCompletionItems(document, position) {
-                const completionItems = functionsWithParameters.map(func => {
+                // Autocomplete for functions
+                const functionCompletionItems = functionsWithParameters.map(func => {
                     const item = new vscode.CompletionItem(func.name, vscode.CompletionItemKind.Function);
 
                     // Prepare the detail string and the function call with placeholders for the parameters
@@ -29,11 +31,18 @@ function activate(context) {
                     snippet.appendText(')');
                     item.detail = `${func.name}(${detailParts.join(', ')})`;
                     item.insertText = snippet;
-                    
+
                     return item;
                 });
 
-                return completionItems;
+                // Autocomplete for patterns
+                const patternCompletionItems = patterns.map(pattern => {
+                    const item = new vscode.CompletionItem(pattern, vscode.CompletionItemKind.Text);
+                    item.insertText = pattern;
+                    return item;
+                });
+
+                return functionCompletionItems.concat(patternCompletionItems);
             }
         }, '.')); // Trigger completion when '.' is pressed
 }
